082ece3bac3afad28c0b1acbb7acd4b57306218d
syauqy <syauqilenterano@gmail.com>
2019-06-27 20:25:50 +0700

    add new velmod and applying cfl condition to determine dt

--- input-data/show.py
+++ input-data/show.py
@@ -2,11 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 #vel=np.ones([100,100])*4000.0
 #np.savetxt("velocity.txt",vel)
-for i in range(0,2000,10):
+"""
+for i in range(0,2403,100):
     U=np.genfromtxt("output"+str(i)+".txt")
     plt.imshow(U,cmap='gray',vmax=0.1,vmin=-0.1)
     plt.show()
-
-U=np.genfromtxt("rec.txt")
-plt.imshow(U,cmap='gray',vmax=0.1,vmin=-0.1)
+"""
+rec=np.genfromtxt("rec.txt")
+plt.pcolormesh(rec,cmap="gray",vmax=0.05,vmin=-0.05)
 plt.show()
