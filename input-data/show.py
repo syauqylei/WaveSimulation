527d4ee29c49e5731fd06de9ffaecf1772a28459
syauqy <syauqilenterano@gmail.com>
2019-06-27 14:41:15 +0700

    trying create record on model bp2400

--- input-data/show.py
+++ input-data/show.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-vel=np.ones([50,50])*4000
-np.savetxt('velocity.txt',vel)
-for i in range(0,400,10):
+#vel=np.ones([100,100])*4000.0
+#np.savetxt("velocity.txt",vel)
+for i in range(0,2000,10):
     U=np.genfromtxt("output"+str(i)+".txt")
-    plt.imshow(U,cmap='gray',vmax=1,vmin=-1)
+    plt.imshow(U,cmap='gray',vmax=0.1,vmin=-0.1)
     plt.show()
+
+U=np.genfromtxt("rec.txt")
+plt.imshow(U,cmap='gray',vmax=0.1,vmin=-0.1)
+plt.show()
